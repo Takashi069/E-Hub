aa881f3615761b623012a8c49183388b6c7051b3
Uday Krishnan <udaykrishnanvg@gmail.com>
2021-11-26 21:24:40 +0530

    getters setters for employee

--- src/employee/employee.java
+++ src/employee/employee.java
@@ -5,4 +5,37 @@ public class employee {
     private String Employee_join_date;
     private String Domain;
     private int Experience;
+
+    public String getEmployee_ID() {
+        return this.Employee_ID;
+    }
+
+    public void setEmployee_ID(String Employee_ID) {
+        this.Employee_ID = Employee_ID;
+    }
+
+    public String getEmployee_join_date() {
+        return this.Employee_join_date;
+    }
+
+    public void setEmployee_join_date(String Employee_join_date) {
+        this.Employee_join_date = Employee_join_date;
+    }
+
+    public String getDomain() {
+        return this.Domain;
+    }
+
+    public void setDomain(String Domain) {
+        this.Domain = Domain;
+    }
+
+    public int getExperience() {
+        return this.Experience;
+    }
+
+    public void setExperience(int Experience) {
+        this.Experience = Experience;
+    }
+
 }
