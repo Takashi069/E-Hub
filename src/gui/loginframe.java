dc87ed49e1b835126bd9c3b0cbdc038775a36d72
Uday Krishnan <udaykrishnanvg@gmail.com>
2021-11-26 21:13:51 +0530

    Removing Override

--- src/gui/loginframe.java
+++ src/gui/loginframe.java
@@ -56,7 +56,7 @@ public class loginframe extends JFrame implements ActionListener {
     }
 
 
-    @Override
+   
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == loginButton) {
             String userText;
