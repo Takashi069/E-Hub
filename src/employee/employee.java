3f85f80f4547fbf303423bba4137c5790027273e
Uday Krishnan <udaykrishnanvg@gmail.com>
2021-11-26 21:18:31 +0530

    Employee changes

--- src/employee/employee.java
+++ src/employee/employee.java
@@ -1,5 +1,8 @@
 package employee;
 
 public class employee {
-    
+    private String Employee_ID;
+    private String Employee_join_date;
+    private String Domain;
+    private int Experience;
 }
