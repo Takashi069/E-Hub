e283cb1f8410fe25572026c58eacc09002093a09
Akash Harikumar <akashharikumar2013@gmail.com>
2021-11-29 10:21:22 +0530

    Added Details in Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,73 @@
 public class Person {
+    private String name;
+    private String DOB;
+    private String[] Address;
+    private int PINCODE;
+    private String Nationality;
+    private String PhoneNumber;
+
     
+    Person(){
+
+        name = "John Doe";
+        DOB = "01/01/1950";
+        Address = new String[]{"StreetName", "Locality","District","State"};
+        PINCODE = 000000;
+        Nationality = "Indian";
+        PhoneNumber = "+910000000000";
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+    /*
+    getAddress is a string array whose elements are as follows:
+    Address[0] = "Street Name"
+    Address[1] = "Locality"
+    Address[2] = "District"
+    Address[3] = "State"
+    */
+    public String[] getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String[] Address) {
+        this.Address = Address;
+    }
+
+    public int getPINCODE() {
+        return PINCODE;
+    }
+
+    public void setPINCODE(int PINCODE) {
+        this.PINCODE = PINCODE;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
 }
